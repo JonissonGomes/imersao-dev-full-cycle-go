4b2c17658ce3f87a4b4c637d3fd3a6677af861bd
JonissonGomes <joni_gomes@hotmail.com>
2022-02-16 17:30:03 -0300

    Criando função para criar um course no banco

--- infra/repository/course_db.go
+++ infra/repository/course_db.go
@@ -2,8 +2,29 @@ package repository
 
 import (
 	"database/sql"
+	"imersao-dev-full-cycle-go/entity"
 )
 
 type CourseDatabaseRepository struct {
 	Db *sql.DB
+}
+
+func (c CourseDatabaseRepository) Insert(course entity.Course) error {
+	statement, err := c.Db.Prepare(`Insert into courses(id, name, description, status) values(?,?,?,?)`)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = statement.Exec(
+		course.ID,
+		course.Name,
+		course.Description,
+		course.Status,
+	)
+
+	if err != nil {
+		return err
+	}
+	return nil
 }
